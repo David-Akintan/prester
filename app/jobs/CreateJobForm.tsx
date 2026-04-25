159963f61194c4879182e2c2cdd42540b511a176
David-Akintan <akintandavid96@gmail.com>
2026-04-25 22:33:57 +0100

    feat: add ThemeProvider context for theme state management

--- app/jobs/CreateJobForm.tsx
+++ app/jobs/CreateJobForm.tsx
@@ -119,7 +119,7 @@ export function CreateJobForm() {
   ) {
     setForm((f) => {
       const milestones = [...f.milestones];
-      milestones[index] = { ...milestones[index], [field]: value };
+      milestones[index] = { ...milestones[index], [field]: value } as MilestoneFormItem;
       return { ...f, milestones };
     });
   }
